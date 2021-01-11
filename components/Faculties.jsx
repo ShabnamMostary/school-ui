f5c186429446cae055963386650df728f5d9e5b8
shabnam <shabnam.mostary@gmail.com>
2021-01-11 01:44:21 -0500

    added another state variable to store filtered faculty list

--- components/Faculties.jsx
+++ components/Faculties.jsx
@@ -6,17 +6,21 @@ import Faculty from './Faculty'
 export default () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [facultyList, setFacultyList] = useState([])
+  const [filteredFacultyList, setFilteredFacultyList] = useState([])
 
   useEffect(() => {
     async function pullData() {
       const { data } = await axios.get('http://localhost:1337/api/faculty')
 
       setFacultyList(data)
+      setFilteredFacultyList(data)
     }
     pullData()
   }, [])
   useEffect(() => {
+    const filtered = facultyList.filter(faculty => faculty.name.toLowerCase().includes(searchTerm.toLowerCase()))
 
+    setFilteredFacultyList(filtered)
   }, [searchTerm])
 
   return (
@@ -25,7 +29,7 @@ export default () => {
       <div className="subtitle">A searchable list of all our faculty!</div>
       <Search term={searchTerm} setter={setSearchTerm} />
       {
-        facultyList.map(faculty => (<Faculty key={faculty.id} id={faculty.id} name={faculty.name} />))
+        filteredFacultyList.map(faculty => (<Faculty key={faculty.id} id={faculty.id} name={faculty.name} />))
       }
 
     </div>
