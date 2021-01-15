f0a801c69b779db2a3b1015a0dca2ed95a2f70f7
shabnam <shabnam.mostary@gmail.com>
2021-01-15 01:32:42 -0500

    updated style in error & title

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.div `
+  font-size: 24px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`
+
+const Subtitle = styled.div `
+  font-size: 20px;
+`
 
 export default () => (
   <>
-    <div className="title">Meet the Faculty</div>
-    <div className="subtitle">A searchable list of all our faculty.</div>
+    <Title>Meet the Faculty</Title>
+    <Subtitle>A searchable list of all our faculty.</Subtitle>
   </>
 )
