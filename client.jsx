f57639fa19b8a7912c78c2c25042110700d4086d
shabnam <shabnam.mostary@gmail.com>
2021-01-15 19:12:07 -0500

    created Faculty page

--- client.jsx
+++ client.jsx
@@ -4,9 +4,11 @@ import { render } from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import FacultyPage from './pages/Faculties'
 import ErrorPage from './pages/Error'
+import SingleFacultyPage from './pages/Faculty'
 
 render(<BrowserRouter>
   <Switch>
+    <Route path="/faculty" component={SingleFacultyPage} />
     <Route exact path="/" component={FacultyPage} />
     <Route path="*" component={ErrorPage} />
   </Switch>
--- components/Faculty.jsx
+++ components/Faculty.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
 
 const Faculty = styled.div `
   font-size: 20px;
   margin-bottom: 10px;
 `
+const Link = styled(NavLink) `
+  text-decoration: none;
+`
 
 export default ({ id, name }) => (
   <Faculty key={id} className="faculty">
-    {`${name}`}
+    <Link to={`/faculty/${id}`}>
+      {`${name}`}
+    </Link>
   </Faculty>
 )
